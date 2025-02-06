592d4b1a1d3d0965230b7a02e42b9a940abb68fa
Bruno Araujo <brunnoaraujoc@gmail.com>
2025-02-06 12:35:30 -0300

    feat: adicionando limparTrasacoes para limpar o cache e adicionando buscarTransacoes para pegar as estatisticas dos ultimos 60 segundos

--- src/main/java/com/api_itau/transacaoApi/business/services/TransacaoService.java
+++ src/main/java/com/api_itau/transacaoApi/business/services/TransacaoService.java
@@ -3,6 +3,7 @@ package com.api_itau.transacaoApi.business.services;
 import com.api_itau.transacaoApi.controller.dtos.TransacaoRequestDTO;
 import com.api_itau.transacaoApi.infrastructure.exceptions.UnprocessableEntity;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.OffsetDateTime;
@@ -11,13 +12,38 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class TransacaoService {
 
     private final List<TransacaoRequestDTO> listaTransacoes = new ArrayList<>();
 
     public void adicionarTransacao(TransacaoRequestDTO dto) {
+
+        log.info("Iniciado o processamento de gravar transações.");
+
         if(dto.dataHora().isAfter(OffsetDateTime.now())) {
+            log.error("Data e hora da transação maiores que a data e hora atual.");
             throw new UnprocessableEntity("Data e hora da transação maiores que a data e hora atual.");
         }
+        if(dto.valor() < 0) {
+            log.error("O valor da transação não pode ser menor que zero.");
+            throw new UnprocessableEntity("O valor da transação não pode ser menor que zero.");
+        }
+
+        listaTransacoes.add(dto);
+
+        }
+    public void limparTransacoes() {
+        listaTransacoes.clear();
     }
+
+    public List<TransacaoRequestDTO> buscarTransacoes(Integer intervaloBusca) {
+        OffsetDateTime dataHoraIntervalo = OffsetDateTime.now().minusSeconds(intervaloBusca);
+
+        return listaTransacoes.stream()
+                .filter(transacao -> transacao.dataHora().isAfter(dataHoraIntervalo))
+                .toList();
+
+    }
+
 }
