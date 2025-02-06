df01ca78a06a834bee62b6fab639d20e00610401
Bruno Araujo <brunnoaraujoc@gmail.com>
2025-02-06 11:46:03 -0300

    initial commit

---
+++ src/main/java/com/api_itau/transacaoApi/business/services/TransacaoService.java
@@ -0,0 +1,11 @@
+package com.api_itau.transacaoApi.business.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TransacaoService {
+
+
+}
